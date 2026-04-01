b59415b16f2716fa8d53da793d9b6f42f5fdb9ec
MAINUL - X <githubmainul@gmail.com>
2026-04-01 21:18:01 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+// config.js
+// ============================================
+// DEVELOPER INFORMATION
+// ============================================
+
+const YEAR = new Date().getFullYear();
+const OWNER = "MAINUL - X";
+
+export const config = {
+  AUTHOR: "Md. Mainul Islam",
+  OWNER: OWNER,
+  GITHUB: "M41NUL",
+  GITHUB_URL: "https://github.com/M41NUL",
+  WHATSAPP: "+8801308850528",
+  TELEGRAM: "@mdmainulislaminfo",
+  EMAIL: "githubmainul@gmail.com, devmainulislam@gmail.com",
+  LICENSE: "MIT License",
+  YEAR: YEAR,
+  COPYRIGHT: `Copyright (c) ${YEAR} ${OWNER}`
+};
+
+export default config;
